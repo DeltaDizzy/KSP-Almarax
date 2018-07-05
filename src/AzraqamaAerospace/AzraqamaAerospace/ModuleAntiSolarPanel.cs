17270f730e732a91fc0dab4f345c691589ff733d
DeltaDizzy <EJBraswell@gmail.com>
2018-07-05 15:03:13 -0500

    plugin ye

---
+++ src/AzraqamaAerospace/AzraqamaAerospace/ModuleAntiSolarPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AzraqamaAerospace
+{
+    public class ModuleAntiSolarPanel : ModuleDeployableSolarPanel
+    {
+        private void Awake()
+        {
+            //find sun
+            List<CelestialBody> sunBodies = new List<CelestialBody>();
+            sunBodies.Add(FlightGlobals.Bodies.Find(s => s.));
+        }
+
+    }
+}
