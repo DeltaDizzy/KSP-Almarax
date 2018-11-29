d80f30e4680af9e80c1a0e9eaae9afdce5b7f209
DeltaDizzy <EJBraswell@gmail.com>
2018-11-29 17:16:40 -0600

    nice

---
+++ src/AzraqamaAerospace/AzraqamaAerospace/ModuleStockDisphotovoltaicPanel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+namespace AzraqamaAerospace
+{
+    public class ModuleStockDisphotovoltaicPanel : ModuleDeployableSolarPanel
+    {
+        ModuleResourceHandler hand;
+        public double currentFlux;
+
+        public override void OnStart(StartState state)
+        {
+
+            //find sun
+            CelestialBody sun = FlightIntegrator.sunBody;
+        }
+
+        void Update()
+        {
+            if (FlightGlobals.ActiveVessel.directSunlight)
+            {
+                currentFlux = FlightIntegrator.ActiveVesselFI.solarFlux *= FlightIntegrator.ActiveVesselFI.solarFluxMultiplier;
+                hand.UpdateModuleResourceOutputs();
+            }
+        }
+    }
+}
